3f56b9c8d07728f28b10c157bbf711974b00c3f2
Alex Gaskell <alex.gaskell10@gmail.com>
2020-01-06 20:56:43 +0000

    small change

--- Intro_to_ML/coursework/cw1_decision_trees/test_eval.py
+++
@@ -1,9 +0,0 @@
-from classes.DecisionTree import DecisionTree
-from classes.Evaluator import DecisionTreeEvaluator
-import numpy as np
-
-
-np.random.seed(100)
-data_path = "wifi_db/clean_dataset.txt"
-eval = DecisionTreeEvaluator(data_path, k_folds=10, max_depth=100)
-eval.run_cross_validation()
